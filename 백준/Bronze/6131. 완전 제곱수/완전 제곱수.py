4ee995af6b029eb535ab49c660da7a74e8f49ad7
DDINGG <101228233+DDINGGG@users.noreply.github.com>
2024-03-12 19:11:24 +0900

    [Bronze III] Title: 완전 제곱수, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
@@ -2,8 +2,8 @@ import sys
 
 n= int(sys.stdin.readline().rstrip())
 count=0
-for i in range(1,500):
-	for j in range(1,500):
+for i in range(1,501):
+	for j in range(1,i):
 		if i**2 -j**2==n:
 			count+=1
 
