b9fc079bd01c5fa27962264e79daf732a7d68b1c
DDINGG <101228233+DDINGGG@users.noreply.github.com>
2024-03-12 18:30:51 +0900

    [Bronze III] Title: 369, Time: 884 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,12 +1,11 @@
 import sys
 
 n= int(sys.stdin.readline().rstrip())
-clap=['3','6','9']
 count=0
 for i in range(1,n+1):
-	s=list(str(i))
+	s= str(i)
 	for j in s:
-		if j in clap:
+		if j in ['3','6','9']:
 			count+=1
 
 print(count)
