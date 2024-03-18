c9587d777b02a51766fb7334086552c373352e11
DDINGG <101228233+DDINGGG@users.noreply.github.com>
2024-03-19 01:58:31 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -3,8 +3,8 @@ import sys
 n= int(sys.stdin.readline().rstrip())
 s= (sys.stdin.readline().rstrip())
 sum=0
-r=31
-for i in range(len(s)):
-	sum+=((ord(s[i])-96)*(r**i)) % 1234567891
 
-print(sum)
+for i in range(len(s)):
+	sum+=((ord(s[i])-96)*(31**i)) 
+
+print(sum% 1234567891)
