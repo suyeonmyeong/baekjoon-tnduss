db8ad35467a9d97518adaf221ae8f789e15c1e73
DDINGG <101228233+DDINGGG@users.noreply.github.com>
2024-03-27 14:11:08 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,16 @@
+import sys
+
+n= int(sys.stdin.readline().rstrip())
+arr = [[0] * 101 for _ in range(101)] 
+
+for _ in range(n): 
+		a, b = map(int, input().split())
+		for i in range(a, a+10):
+				for j in range(b, b+10):
+						arr[i][j] = 1
+
+res = 0
+for i in arr: 
+		res += sum(i)
+
+print(res) 
