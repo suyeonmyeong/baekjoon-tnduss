3ebf0f8898c9c7f4268c37d34b9e95a6130aca7b
DDINGG <101228233+DDINGGG@users.noreply.github.com>
2024-04-01 02:24:54 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+temp = []
+
+
+def dfs(start):
+		if len(temp) == m:
+				print(*temp)
+				return
+		for i in range(start, n):
+				if nums[i] not in temp:
+						temp.append(nums[i])
+						dfs(i + 1)
+						temp.pop()
+
+dfs(0)
