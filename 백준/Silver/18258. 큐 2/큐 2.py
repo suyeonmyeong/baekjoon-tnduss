5522264b71d8f359b97ecc3b7fa0da1c49b436e5
DDINGG <101228233+DDINGGG@users.noreply.github.com>
2024-04-02 01:23:12 +0900

    [Silver IV] Title: 큐 2, Time: 2168 ms, Memory: 110624 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+
+n= int(sys.stdin.readline().rstrip())
+
+def isempty(l):
+	if l==0:
+		return True
+
+num=deque()
+for i in range(n):
+	lst=list(sys.stdin.readline().rstrip().split())
+	l=len(num)
+	match lst[0]:
+		case 'push':
+			num.append(int(lst[1]))
+		case 'pop':
+			if isempty(l):
+				print(-1)
+			else:
+				print(num.popleft())
+		case 'size':
+			print(l)
+		case 'empty':
+			if isempty(l):
+				print(1)
+			else:
+				print(0)
+		case 'front':
+			if isempty(l):
+				print(-1)
+			else:
+				print(num[0])
+		case 'back':
+			if isempty(l):
+				print(-1)
+			else:
+				print(num[-1])
